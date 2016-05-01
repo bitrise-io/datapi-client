adc7b015d558ea426117ff5bf65ef3bd227dafe7
Viktor Benei <viktor.benei@gmail.com>
2016-05-01 18:14:55 +0200

    base "send data" with test - done

--- lib/datapi/client.rb
+++ lib/datapi/client.rb
@@ -1,7 +1,34 @@
-require "datapi/client/version"
+require 'datapi/client/version'
+require 'json'
+require 'net/http'
 
 module Datapi
   module Client
-    # Your code goes here...
+    #
+    class StoreData
+      def initialize(datapi_root_url, datapi_read_write_api_token)
+        @datapi_root_url = datapi_root_url
+        @datapi_read_write_api_token = datapi_read_write_api_token
+      end
+
+      def send(data, typeid, generated_at = nil)
+        generated_at = DateTime.now if generated_at.nil?
+
+        uri = URI.join(@datapi_root_url, '/data/', typeid)
+        req = Net::HTTP::Post.new(
+          uri.path,
+          { 'Content-Type' => 'application/json',
+            'Authorization' => "Token token=#{@datapi_read_write_api_token}"
+          })
+        req.body = { data: data, generated_at: generated_at }.to_json
+
+        res = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme.eql?('https')) do |http|
+          http.request(req)
+        end
+
+        resp_data = JSON.parse(res.body)
+        return resp_data
+      end
+    end
   end
 end
--- spec/datapi/client_spec.rb
+++ spec/datapi/client_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe Datapi::Client do
+  before :each do
+    datapi_root_url = ENV['DATAPI_SERVER_URL']
+    raise 'No DATAPI_SERVER_URL provided!' if datapi_root_url.to_s.empty?
+    datapi_read_write_api_token = ENV['DATAPI_READ_WRITE_API_TOKEN']
+    raise 'No DATAPI_READ_WRITE_API_TOKEN provided!' if datapi_read_write_api_token.to_s.empty?
+
+    @datapi_store = Datapi::Client::StoreData.new(datapi_root_url, datapi_read_write_api_token)
+  end
+
   it 'has a version number' do
     expect(Datapi::Client::VERSION).not_to be nil
   end
 
   it 'does something useful' do
     expect(true).to eq(true)
+  end
+
+  it 'send data' do
+    sample_data = { 'label' => 'sample_data', 'value' => true }
+    resp_data = @datapi_store.send(sample_data, 'unit-test')
+    expect(resp_data['item']['data']).to eq(sample_data)
   end
 end
