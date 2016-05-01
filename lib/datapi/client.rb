26fe91015c8475e80a4db3ae5191bee18232c09c
Viktor Benei <viktor.benei@gmail.com>
2016-05-01 19:02:15 +0200

    GetData

--- lib/datapi/client.rb
+++ lib/datapi/client.rb
@@ -30,5 +30,28 @@ module Datapi
         return resp_data
       end
     end
+    #
+    class GetData
+      def initialize(datapi_root_url, datapi_readonly_api_token)
+        @datapi_root_url = datapi_root_url
+        @datapi_readonly_api_token = datapi_readonly_api_token
+      end
+
+      def get_data_by_typeid(typeid)
+        uri = URI.join(@datapi_root_url, '/data/', typeid)
+        req = Net::HTTP::Get.new(
+          uri.path,
+          { 'Content-Type' => 'application/json',
+            'Authorization' => "Token token=#{@datapi_readonly_api_token}"
+          })
+
+        res = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme.eql?('https')) do |http|
+          http.request(req)
+        end
+
+        resp_data = JSON.parse(res.body)
+        return resp_data
+      end
+    end
   end
 end
--- spec/datapi/client_spec.rb
+++ spec/datapi/client_spec.rb
@@ -1,26 +1,53 @@
 require 'spec_helper'
 
 describe Datapi::Client do
-  before :each do
-    datapi_root_url = ENV['DATAPI_SERVER_URL']
-    raise 'No DATAPI_SERVER_URL provided!' if datapi_root_url.to_s.empty?
-    datapi_read_write_api_token = ENV['DATAPI_READ_WRITE_API_TOKEN']
-    raise 'No DATAPI_READ_WRITE_API_TOKEN provided!' if datapi_read_write_api_token.to_s.empty?
-
-    @datapi_store = Datapi::Client::StoreData.new(datapi_root_url, datapi_read_write_api_token)
-  end
-
   it 'has a version number' do
     expect(Datapi::Client::VERSION).not_to be nil
   end
 
+  it 'required test Env Vars are set' do
+    datapi_root_url = ENV['DATAPI_SERVER_URL']
+    raise 'No DATAPI_SERVER_URL provided!' if datapi_root_url.to_s.empty?
+    datapi_read_write_api_token = ENV['DATAPI_READ_WRITE_API_TOKEN']
+    raise 'No DATAPI_READ_WRITE_API_TOKEN provided!' if datapi_read_write_api_token.to_s.empty?
+    datapi_readonly_api_token = ENV['DATAPI_READONLY_API_TOKEN']
+    raise 'No DATAPI_READONLY_API_TOKEN provided!' if datapi_readonly_api_token.to_s.empty?
+  end
+
   it 'does something useful' do
     expect(true).to eq(true)
+  end
+end
+
+describe Datapi::Client::StoreData do
+  before :each do
+    datapi_root_url = ENV['DATAPI_SERVER_URL']
+    datapi_read_write_api_token = ENV['DATAPI_READ_WRITE_API_TOKEN']
+
+    @datapi_store = Datapi::Client::StoreData.new(datapi_root_url, datapi_read_write_api_token)
   end
 
   it 'send data' do
     sample_data = { 'label' => 'sample_data', 'value' => true }
     resp_data = @datapi_store.send(sample_data, 'unit-test')
     expect(resp_data['item']['data']).to eq(sample_data)
+  end
+end
+
+describe Datapi::Client::GetData do
+  before :each do
+    datapi_root_url = ENV['DATAPI_SERVER_URL']
+    datapi_readonly_api_token = ENV['DATAPI_READONLY_API_TOKEN']
+
+    @datapi_get = Datapi::Client::GetData.new(datapi_root_url, datapi_readonly_api_token)
+  end
+
+  it 'get items' do
+    resp_data = @datapi_get.get_data_by_typeid('unit-test')
+    resp_items = resp_data['items']
+
+    expect(resp_items).not_to be_nil
+    expect(resp_items).to be_an_instance_of(Array)
+    expect(resp_items.length).to be >= 1
   end
 end
