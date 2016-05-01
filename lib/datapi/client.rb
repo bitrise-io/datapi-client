7f58dcb3de700ab444a41a76770d429e846f97b9
Viktor Benei <viktor.benei@gmail.com>
2016-05-01 17:07:29 +0200

    init

---
+++ lib/datapi/client.rb
@@ -0,0 +1,7 @@
+require "datapi/client/version"
+
+module Datapi
+  module Client
+    # Your code goes here...
+  end
+end
---
+++ lib/datapi/client/version.rb
@@ -0,0 +1,5 @@
+module Datapi
+  module Client
+    VERSION = "0.1.0"
+  end
+end
